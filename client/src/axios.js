0f3c7b86b002fa79ba780ff427831270032c7b7c
YN1kiforov <yakov-nikiforov.2@mail.ru>
2022-10-26 20:39:42 +0700

    deploy

--- client/src/axios.js
+++ client/src/axios.js
@@ -1,5 +1,5 @@
 import axios from "axios";
 const instance = axios.create({
-	baseURL: 'http://localhost:3001/',
+	baseURL: 'https://sheltered-castle-49133.herokuapp.com/',
 });
 export default instance
